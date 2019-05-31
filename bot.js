73dfb47dcaee6190ff88926bd6b6881575b411c0
Glitch (dour-wizard) <none>
2019-05-31 04:23:24 +0000

    Ask question with choices in one message rather than all separate

--- bot.js
+++ bot.js
@@ -7,12 +7,12 @@ const { MongoDbStorage } = require('botbuilder-storage-mongodb');
 
 var request = require('request');
 var atob = require('atob');
-var results;
+
 var choices;
 var correct;
-var res;
 
-require('dotenv').config();
+
+//require('dotenv').config();
 let storage = null;
 if (process.env.MONGO_URI) {
     storage = mongoStorage = new MongoDbStorage({
@@ -37,7 +37,7 @@ const controller = new Botkit({
 
 controller.on('message', async(bot, message) => {
      if(message.text){
-       console.log(message);
+       //console.log(message);
        const query = message.text.trim();
        const email = message.personEmail;
        const firstName_lower = email.substr(0, email.indexOf('.'));
@@ -47,28 +47,31 @@ controller.on('message', async(bot, message) => {
          await bot.reply(message, 'Usage: @trivia hit me');
       }    
       else if(query.includes('hit me')){
-        res = null;
         let response = await fetch('https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple&encode=base64&token=6893a0d72cdc7f72d0268acd92f779a96e5e5b21b5bea51411d4f172cb15ddec');
         response = await response.json();
    
-        results = response.results[0]
-        var decode =  atob(results.question);
-        await bot.reply(message, "Question: " + decode);
+        let results = response.results[0]
+        let decoded_question =  atob(results.question);
+        let question = decoded_question.charAt(0).toLowerCase() + decoded_question.slice(1);
+        //await bot.reply(message, "Question: " + decode);
         correct = [atob(results.correct_answer)];
-        var incorrect = [atob(results.incorrect_answers[0]), atob(results.incorrect_answers[1]), atob(results.incorrect_answers[2])];
+        let incorrect = [atob(results.incorrect_answers[0]), atob(results.incorrect_answers[1]), atob(results.incorrect_answers[2])];
 
         choices = correct.concat(incorrect);
 
         choices = shuffle(choices);
-         var i;
-         for(i = 0; i < choices.length; i++){
-            await bot.reply(message, (i+1) + ". " + choices[i]);
+        let question_string = firstName + ", " + question + "\n\n";
+        question_string += "1. " + choices[0];
+        for(let i = 1; i < choices.length; i++){
+            question_string = question_string + "\n" + (i+1) + ". " + choices[i];
+            //await bot.reply(message, (i+1) + ". \n" + choices[i]);
          }
+        await bot.reply(message, question_string);
       }
        else if(query.includes('answer')){
          let correct_answer = "";
          correct_answer = correct[0];
-         var correctNum = choices.indexOf(correct_answer) + 1;
+         let correctNum = choices.indexOf(correct_answer) + 1;
          await bot.reply(message, "Answer: " + correctNum + ". " + correct_answer);
        }
      }
