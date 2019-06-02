0be9147d8c7b559dfb2c2508c3b5b729ef45546a
Glitch (dour-wizard) <none>
2019-06-02 22:21:48 +0000

    Stores room id in Mongo database

--- bot.js
+++ bot.js
@@ -2,8 +2,7 @@
 const { Botkit } = require('botkit');
 const { WebexAdapter } = require('botbuilder-adapter-webex');
 const { BotkitCMSHelper } = require('botkit-plugin-cms');
-const { MongoDbStorage } = require('botbuilder-storage-mongodb');
-//const JSON = require('json');
+const mongodb = require('mongodb')
 
 var request = require('request');
 var atob = require('atob');
@@ -11,15 +10,17 @@ var atob = require('atob');
 var choices;
 var correct;
 var letters = "ABCD";
+//var dbSongs = ""
+
+
+// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname, details set in .env
+
+                            
+                            
 
 
 //require('dotenv').config();
-let storage = null;
-if (process.env.MONGO_URI) {
-    storage = mongoStorage = new MongoDbStorage({
-        url : process.env.MONGO_URI,
-    });
-}
+//let storage = ;
 
 const numQuestions = 5;
 const adapter = new WebexAdapter({
@@ -32,23 +33,27 @@ const adapter = new WebexAdapter({
 const controller = new Botkit({
     webhook_name: 'TriviaBot',
     adapter: adapter,
-    storage
+   // storage: mongodb
 
 });
 
 controller.on('message', async(bot, message) => {
      if(message.text){
-      // console.log(message);
+       console.log(message);
+       getRoom(message);
+       console.log
        const query = message.text.trim();
        const email = message.personEmail;
-       const firstName_lower = email.substr(0, email.indexOf('.'));
-       const firstName = firstName_lower.charAt(0).toUpperCase() + firstName_lower.slice(1)
+       const id = message.personId;
+       const firstNameLower = email.substr(0, email.indexOf('.'));
+       const firstName = firstNameLower.charAt(0).toUpperCase() + firstNameLower.slice(1)
        if(query.includes('help')) {
-         let help_message = ""
-         help_message += "Hi, " + firstName + "! I'm Trivia Timmy! I will ask trivia questions and you will have to choose from the 4 choices given.\n\n";
-         help_message += "To get a question: @Trivia hit me <category number> (optional)\n";
-         help_message += "To see the categories: @Trivia categories\n";
-         await bot.reply(message, help_message);
+         let helpMessage = ""
+         helpMessage += "Hi, " + firstName + "! I'm Trivia Timmy! I will ask trivia questions and you will have to choose from the 4 choices given.\n\n";
+         helpMessage += "To get a question: @Trivia hit me <category number> (optional)\n";
+         helpMessage += "To answer a question: @Trivia answer <letter choice>\n"
+         helpMessage += "To see the categories: @Trivia categories\n";
+         await bot.reply(message, helpMessage);
       }
       else if(query.includes('categories')){
          let response = await fetch('https://opentdb.com/api_category.php');
@@ -74,8 +79,8 @@ controller.on('message', async(bot, message) => {
    
         let results = response.results[0]
         //console.log(results);
-        let decoded_question =  atob(results.question);
-        let question = decoded_question.charAt(0).toLowerCase() + decoded_question.slice(1);
+        let question =  atob(results.question);
+       // let question = decodedQuestion.charAt(0).toLowerCase() + decoded_question.slice(1);
         //await bot.reply(message, "Question: " + decode);
         correct = [atob(results.correct_answer).trim()];
         let incorrect = [atob(results.incorrect_answers[0].trim()), atob(results.incorrect_answers[1].trim()), atob(results.incorrect_answers[2].trim())];
@@ -83,51 +88,73 @@ controller.on('message', async(bot, message) => {
         choices = correct.concat(incorrect);
 
         choices = shuffle(choices);
-        let question_string = firstName + ", " + question + "\n";
+        let questionString = firstName + ", " + question + "\n";
         //question_string += "1. " + choices[0];
         for(let i = 0; i < choices.length; i++){
-            question_string = question_string + "\n" + letters.charAt(i) + ". " + choices[i];
+            questionString = questionString + "\n" + letters.charAt(i) + ". " + choices[i];
             //await bot.reply(message, (i+1) + ". \n" + choices[i]);
          }
-        await bot.reply(message, question_string);
+        await bot.reply(message, questionString);
       }
-//         else if(query.includes('anime me')){
-//         let response = await fetch('https://opentdb.com/api.php?amount=1&category=31&type=multiple&encode=base64');
-//         response = await response.json();
-   
-//         let results = response.results[0]
-//         //console.log(results);
-//         let decoded_question =  atob(results.question);
-//         let question = decoded_question.charAt(0).toLowerCase() + decoded_question.slice(1);
-//         //await bot.reply(message, "Question: " + decode);
-//         correct = [atob(results.correct_answer).trim()];
-//         let incorrect = [atob(results.incorrect_answers[0].trim()), atob(results.incorrect_answers[1].trim()), atob(results.incorrect_answers[2].trim())];
-
-//         choices = correct.concat(incorrect);
-
-//         choices = shuffle(choices);
-//         let question_string = firstName + ", " + question + "\n";
-//         //question_string += "1. " + choices[0];
-//         for(let i = 0; i < choices.length; i++){
-//             question_string = question_string + "\n" + letters.charAt(i) + ". " + choices[i];
-//             //await bot.reply(message, (i+1) + ". \n" + choices[i]);
-//          }
-//         await bot.reply(message, question_string);
-//       }
        else if(query.includes('answer')){
          let selectedChoice = query.slice(query.indexOf('answer') + 'answer'.length).trim();
          //let correctAnswer = "";
          let correctAnswer = correct[0];
          let correctLetter = letters.charAt(choices.indexOf(correctAnswer));
          if(selectedChoice === correctLetter) {
-           await bot.reply(message, "Good job, " + firstName + ", " + correctAnswer + " is correct!")
+           await bot.reply(message, "Good job, " + firstName + ", " + correctLetter + " - " + correctAnswer + " is correct!")
 }
          else {
-           await bot.reply(message, "Sorry, " + firstName + ", that is incorrect. The correct answer is " + correctAnswer + ".");
+           await bot.reply(message, "Sorry, " + firstName + ", that is incorrect. The correct answer is " + 
+                           correctLetter + " - " + correctAnswer + ".");
          }
        }
      }
 });
+
+function getRoom(message) {
+  var uri = "mongodb+srv://" + process.env.dbuser + ":" + process.env.dbpassword + "@cluster0-vblnv.mongodb.net/trivia?retryWrites=true&w=majority";
+
+
+  mongodb.MongoClient.connect(uri, function(err, db) {
+    if(err) throw err;
+    const triviaDatabase = db.db('trivia')
+    var rooms = triviaDatabase.collection('rooms');
+   //  rooms.drop();
+    var room = [
+      {
+        roomId: message.roomId,
+        currentQuestion: '',
+        currentAnswer: '',
+        currentPlayer: '',
+        allPlayers:''
+      }
+    ]
+
+    console.log("Room id: " + message.roomId)
+
+    rooms.insert(room, function(err, result) {
+
+      if(err) throw err;
+
+      console.log("insertion success")
+
+      rooms.find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+      });
+      console.log("second test")
+      rooms.find({roomId: message.roomId}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log("Found room id: " + message.roomId);
+        db.close();
+      });
+    });
+  });
+}
+
+
 
 function shuffle(a) {
     var j, x, i;
